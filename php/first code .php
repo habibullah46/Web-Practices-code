121ee4c0444350e35bd09f449d74252f5e67f4fc
Habibullah <103850736+habibullah46@users.noreply.github.com>
2023-10-21 21:36:52 +0500

    new web

---
+++ php/first code .php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php
+   function className($CName)
+   {
+      echo"$CName classmate.<br>";
+   }
+   className("habibullah");
+   className("Azkar ul hassan");
+   className("Hammad");
+   className("Shoukat Rehman");
+   function TeachersName($TName)
+   {
+      echo "$TName Teacherain.<br>";
+   }
+   TeachersName("Farooq akhtar");
+   TeachersName(" shujaat Rathour");
+   TeachersName("madam Naila");
+   TeachersName("Ali khan");
+?>
+</head>
+<body>
+    
---
+++ php/php code.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <?php
+echo "well come";
+?>
+</head>
+<body>
+    
